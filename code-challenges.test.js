f1f17d76fa442dc8f93419555a992f12770db395
aribrashear <aribrashear@gmail.com>
2022-09-23 21:27:45 -0700

    initial commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,54 +14,182 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("fibFunction", () => {
+  it(`Takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.`, () => {
+    const fibLength1 = 6;
+    const fibLength2 = 10;
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(fibFunction(fibLength1)).toEqual([1, 1, 2, 3, 5, 8]);
+    expect(fibFunction(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+    expect(fibFunction(0)).toEqual(
+      "Please enter a number that's at least 2, or higher."
+    );
+  });
+});
+
+// GOOD FAIL: fibfunction is not defined
 
 // b) Create the function that makes the test pass.
+/*
+--------- FUNCTION INFORMATION:
+Describe what the function will do: Function will take in a number and return an array of numbers in the fibonacci sequence that matches the length of the number.
+Function Name: fibFunction
+Function Parameter(s): 1, number
+--------- INPUT / OUTPUT:
+input 1: number
+output 1: array of numbers in fibonacci sequence
+--------- STEPS/METHODS:
+1. I'll declare an array to store the first two values of the fibonacci sequence, as well as an array with [1] to start us off.
+2. I'll use a conditional statement to ensure the number being entered is greater than 2. If it isn't, I'll return a string notifying that the number needs to be at least 2 or higher.
+3. I'll use a for loop to iterate through the fibonacci sequence, and return the corresponding output to an array of the specified length.
+4. Return the resulting array.
+*/
+
+// The Fibonacci sequence is the integer sequence where the first two terms are 0 and 1. After that, the next term is defined as the sum of the previous two terms.
+
+const fibFunction = (n) => {
+  // The first two terms in the fibonacci sequence are 0 and 1, so I'm creating an array to store those values.
+  let fib = [0, 1];
+  // Starting the array that the sequence will be stored in with a 1.
+  let newFib = [1];
+
+  if (n > 0) {
+    // Looping to populate an array that will be the length of the specified input.
+
+    for (let i = 2; i <= n; i++) {
+      // Current index = (index - 2) + (index -1)
+      // Starts at index 2, so this will start iterating with 0 + 1, then 1 + 1, 2 + 1, etc.
+      fib[i] = fib[i - 2] + fib[i - 1];
+      // Push the added total to the current index of newFib.
+      newFib.push(fib[i]);
+    }
+    // Return the total populated array.
+    return newFib;
+  } else if (n < 2) {
+    // If number entered is less than 2, return this string:
+    return "Please enter a number that's at least 2, or higher.";
+  } else {
+    // Edge case, because you never know. (:
+    return "Oops! Something went wrong.";
+  }
+};
 
 // --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+describe("objSort", () => {
+  it(`Takes in an object and returns an array of the numbers sorted from least to greatest.`, () => {
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60,
+    };
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+    const studyMinutesWeek2 = {
+      sunday: 100,
+      monday: 10,
+      tuesday: 45,
+      wednesday: 60,
+      thursday: 20,
+      friday: 15,
+      saturday: 65,
+    };
+
+    expect(objSort(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90]);
+    expect(objSort(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100]);
+  });
+});
+
+// GOOD FAIL: ReferenceError: objSort is not defined
 
 // b) Create the function that makes the test pass.
+
+/*
+--------- FUNCTION INFORMATION:
+Describe what the function will do: Function will accept a parameter of an object, and returns an array of the numbers sorted from least to greatest.
+Function Name: objSort
+Function Parameter(s): 1, object w/number values
+--------- INPUT / OUTPUT:
+input 1: object w/number values
+output 1: array w/number values sorted from least to greatest.
+
+--------- STEPS/METHODS:
+1. Declare a temporary variable (newArr), which we will be passing the values from our object into.
+2. Use the method Object.values() to obtain all values from the object being passed in.
+3. Use the .sort method with an argument of ((a, b) => a - b) in order to sort the items in the array from least to greatest.
+4. Return newArr.
+*/
+
+const objSort = (object) => {
+  // Filling the empty arr with the values of object, then sorting them.
+  newArr = Object.values(object).sort((a, b) => a - b);
+  // Returning the sorted array.
+  return newArr;
+};
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+describe("arrSummation", () => {
+  it(`Takes in an array and returns an array of the accumulating sum.`, () => {
+    const accountTransactions1 = [100, -17, -23, -9];
+    const accountTransactions2 = [250, -89, 100, -96];
+    const accountTransactions3 = [];
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+    expect(arrSummation(accountTransactions1)).toEqual([100, 83, 60, 51]);
+    expect(arrSummation(accountTransactions2)).toEqual([250, 161, 261, 165]);
+    expect(arrSummation(accountTransactions3)).toEqual([]);
+  });
+});
 
-const accountTransactions3 = []
-// Expected output: []
+// GOOD FAIL: arrSummation is not defined
 
 // b) Create the function that makes the test pass.
+/*
+--------- FUNCTION INFORMATION:
+Describe what the function will do: Function will take in an array and return an array of the accumulating sum.
+Function Name: arrSummation
+Function Parameter(s): 1, array of numbers
+--------- INPUT / OUTPUT:
+input 1: arr of numbers
+output 1: arr of same length, with each element returning the sum of adding it to the previous element.
+
+input 2: empty array
+output 2: []
+--------- STEPS/METHODS:
+1. Declare a variable to hold our starting number, which will be set to 0.
+2. Use the ternary operator to check if the array.length is greater than 0, so that it will return an empty array if the one passed in has no value.
+3. Since I want to do something to EVERY element in the array AND return an array of the same length, I'll use .map() to iterate.
+4. Add the value of the current iteration to the value stored in the temporary variable, reset the value of the temporary variable with the current value, then return the value to the array.
+*/
+
+// ----- VISUAL BREAKDOWN -----
+// Expect: [100, 83, 60, 51]
+// 0. 100
+// 1. 100 - 17 = 83
+// 2. 83 - 23 = 60
+// 3. 60 - 9 = 51
+
+const arrSummation = (arr) => {
+  // Setting the starting value of our temporary variable to 0. This will be updated as .map() runs and stores the result of adding the value to it.
+  let theAccumulator = 0;
+
+  // If the array is empty, return an empty array.
+  return arr.length === 0
+    ? []
+    : arr.map((v) => {
+        // Setting the current value to: (value) + (the value stored in theAccumulator)
+        v = v + theAccumulator;
+        // Setting theAccumulator to store the current value so it can be used in the next iteration.
+        theAccumulator = v;
+        // Returning the value to the array.
+        return v;
+      });
+};
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,71 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX stands for Javascript XML. It's essentially a combination of HTML and Javascript, and was created specifically for React. So, while you will use HTML tags when writing JSX, there are syntactical differences. Whenever you're trying to use raw JS in your code, it always needs to be inside of curly braces {} so that the program knows how to parse it. For an example, inline styling in raw HTML would look like this:
 
-Researched answer:
+              - It's more conventional to use underscores for things like class names when using raw HTML.
+              - Notice that there are no curly braces here, and the style values are not in quotes.
+
+<div class="empty_box" style="color: blue, font-size: 12px;">Example Text</div>
+
+And JSX styling would look more like this:
+
+              - It's more conventional to use camelCase for things like class/variable names while using JSX.
+              - Notice the curly braces enclosing the style values, as well as the quotes to notate strings.
+
+<div className="emptyBox" style{{color: "blue", fontSize: "12px"}}>Example Text</div>
+
+Ultimately, HTML and JS are working together when you're using JSX, to allow you access to the best of both worlds while you're creating your web applications. (:
+
+Researched answer: In addition to my previous answer, it's good to know that JSX is compiled into regular Javascript when it's run, so the browser itself only recognizes both the raw HTML and the raw Javascript. It has no context for what JSX is!
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-Your answer:
+Your answer: Yarn is a package manager that we use as developers to access the collaborative code of other developers, so that we can create smoother applications, faster. I've previously used yarn for jest testing, but I've also used it to compile the files necessary for React apps. When you run yarn to create a new React app, it will add a folder called node_modules which contains all of the source code that allows the application to function, as well as a public and src folder that contain various components like your index.html file and App.js.
 
-Researched answer:
+Researched answer: In addition to my previous answer, yarn stands for "Yet Another Resource Negotiator", and was created by Facebook in 2016 for Node.js.
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function in Javascript is, as the name implies, a function that has no name. Probably one of the places you'll see them used the most is in methods, which will work on specific data types like objects and arrays. For example, in the array method .map(), you will pass in an anonymous function that will accept up to three parameters. (array.map((value, index, origArr) => )).
 
-Researched answer:
+Researched answer: In addition to my previous answer, it's important to note that anonymous functions CANNOT be called after their creation, as there would be no name to call it by. They can also accept multiple arguments, but can only have one expression.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: In react, state values are values that can be updated in real-time via the Virtual DOM without refreshing the full page. The format for that looks like this:
+const [state, setState] = useState(initialValue)
+They're great for storing data that we will want to update later!
+Props come in handy when you have multiple components (isolated code stored on another file which will be connected and rendered on your main page) that you want to give access to that state. We do this through the use of props (short for properties), which we can then pass down to a child component like so:
+<Component dataName="data"/>
+That data can then be accessed from the child component, like so:
+const Component = (props) => {
 
-Researched answer:
+<div>{props.dataName}</div>
+}
+
+Researched answer: In addition to my previous answer, it's important to note that states and props are both JS objects. This means that to access data that's being stored inside of them, we need to remember to either use dot notation, or destructure that data at the top of our code to be accessed later.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM stands for Document Object Model, and it's the visual representation of code that we can see displayed on a webpage. There is also something in React called the Virtual DOM, which is incredibly useful. That's because normally, the entire DOM needs to be refreshed to update changes that have occured on the page, but when we use the Virtual DOM inside of React we're able to use "states" that can be refreshed
 
-Researched answer:
+Researched answer: In addition to my previous answer, you can view the DOM almost like a tree. Each HTML element on the page will be a node, with tags like <body> and <head> acting as the parents to the elements inside of them.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: Both div and span elements in HTML represent empty containers that are used to store and style content, but the key difference between them is their block level. Div elements are block-level elements, and span elements are inline-block elements. This matters when it comes to styling, as everything you see displayed on a page is essentially being stored inside of a bunch of boxes. For example, you may have a block of text stored inside of a <div> element. If you were to change the background color of that <div>, the entire "block" of text would change color. However, if you wrapped one sentence inside of the paragraph in a <span> tag, you could then change the background color of just that line of text.
 
 Researched answer:
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming is a type of programming that primarily relies on classes and objects. Examples of object-oriented programming include JS, Ruby, C++, and Python.
 
-2. Ruby:
+2. Ruby: Ruby is an object-oriented programming language that, like JS, is interpreted at runtime.
 
-3. Implicit return:
+3. Implicit return: An implicit return is one that's returned without the use of a "return" keyword, usually taking place on a single line.
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are anonymous functions that we're able to pass into methods.
 
-5. Ruby hashes:
+5. Ruby hashes: A ruby hash is a data structure similar to Objects in JS, with collections of key:value pairs.
